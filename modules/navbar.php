36c1e45f0eef411e338a32f7b9c6be98cd428d5d
Wesley PIN <wpin@itop.local>
2019-05-20 16:13:11 +0200

    modif navbar

---
+++ modules/navbar.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+    <div class="navbar-block">
+        <!-- <div class="navbar-block-logo">
+            <img src="img/wtfLogo.png">
+        </div> -->
+        <div class="navbar-block-lien">
+            <button>Réservation</button>
+            <button>Connexion</button>
+        </div>
+    </div>
+</div>
