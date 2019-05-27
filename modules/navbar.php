c4a9278fb367b32921d63d8e49698b215b8c0cb1
Wesley PIN <wpin@itop.local>
2019-05-27 13:41:31 +0200

    Navbar style

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,12 +1,7 @@
 <div class="navbar">
     <div class="navbar-block">
-        <div class="navbar-block-logo">
-            <!-- <span>Logo</span> -->
-            <img src="../img/wtfLogo.png">
-        </div>
-        <div class="navbar-block-lien">
-            <a href="accueil.php?page=resa">Réservation</a>
-            <a href="accueil.php?page=connexion">Connexion</a>
-        </div>
+        <a href="accueil.php?page=resa">Réservation</a>
+        <a href="../index.php"><img src="../img/planeLogo.png"></a>
+        <a href="accueil.php?page=connexion">Connexion</a>
     </div>
 </div>
