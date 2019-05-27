72dd43e55e0c56dc078b47f0f331e00064974edf
BastienHtn <bastien.haton@gmail.com>
2019-05-27 13:45:16 +0200

    Modifs animations Index

--- index.php
+++ index.php
@@ -16,45 +16,51 @@ if(session_status() == PHP_SESSION_NONE)
             }
 
             #logoWtf{
-                max-height: 35vw;
+                max-height: 17vw;
                 width: auto;
+                margin-left: auto;
+                margin-right: auto;
+                margin-top: 3vw;
+                display: block;
             }
 
             #logoIntro{
                 animation-name: intro;
-                animation-duration: 4s;
-                margin-left: 25vw;
+                animation-duration: 3s;
             }
 
             @keyframes intro{
-                from {opacity: 0; margin-left: -100vw;}
-                to {opacity: 1; margin-left: 25vw;}
+                from {opacity: 0; margin-left: -100%; margin-right: 100%;}
+                to {opacity: 1;}
             }
 
             #infos{
                 text-align: center;
                 color: white;
-                font-size: 50px;
+                font-size: 2vw;
                 animation-name: introText;
+                animation-duration: 3s;
+                animation-delay: 1s;
             }
 
             @keyframes introText{
-                from {opacity: 0; font-size: 2px;}
-                to {opacity: 1; font-size: 50px;}
+                from {opacity: 0;}
+                to {opacity: 1;}
             }
         </style>
+    </head>
+
+    <body>
+        <div id="logoIntro">
+            <img id="logoWtf" src="img/wtfLogo.png">
+        </div>
+        <div id="infos" class="display-4">
+            Un outil de gestion de voyages, que vous soyez seul ou accompagné...</br></br>
+            <button type="button" class="btn btn-secondary" style="font-size: x-large;">Créer mon Voyage</button>
+        </div>
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </head>
-    <body>
-    <div id="logoIntro">
-        <img id="logoWtf" src="img/wtfLogo.png">
-    </div>
-    <div id="infos">
-        Un outil de gestion de voyages, seul ou accompagné...
-    </div>
-        <a href="modules/accueil.php?page=connexion">Accueil -></button>
     </body>
 </html>
