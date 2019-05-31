aa693809e1b502201d246de58797c1ff9c06cb54
Wesley PIN <wpin@itop.local>
2019-05-31 17:24:34 +0200

    [IHM] Fonctionnel étape 2 et style carré étape 1

--- modules/resa.php
+++ modules/resa.php
@@ -34,33 +34,37 @@
                 <div class="etape2-block">
                     <h2>Sélectionnez vos centres d'intérêts</h2>
                     <div class="etape2-block-interet">
-                        <div style="position: relative;">
+                        <div style="position: relative;" id="interet-food" onclick="etape2_choose('interet-food')">
                             <img src="https://img.icons8.com/color/100/000000/food-and-wine.png">
-                            <img class="icon-validate" src="https://img.icons8.com/cotton/64/000000/cancel.png">
+                            <img id="interet-food-x" class="icon-validate" src="https://img.icons8.com/cotton/64/000000/cancel.png">
+                            <img id="interet-food-v" class="icon-validate desactive" src="https://img.icons8.com/cotton/64/000000/checkmark.png">
                         </div>
                         <br>
                         <span>Gastronomie</span>
                     </div>
                     <div class="etape2-block-interet">
-                        <div style="position: relative;">
+                        <div style="position: relative;" id="interet-landscape" onclick="etape2_choose('interet-landscape')">
                             <img src="https://img.icons8.com/color/100/000000/alps.png">
-                            <img class="icon-validate" src="https://img.icons8.com/cotton/64/000000/cancel.png">
+                            <img id="interet-landscape-x" class="icon-validate" src="https://img.icons8.com/cotton/64/000000/cancel.png">
+                            <img id="interet-landscape-v" class="icon-validate desactive" src="https://img.icons8.com/cotton/64/000000/checkmark.png">
                         </div>
                         <br>
                         <span>Paysage</span>
                     </div>
                     <div class="etape2-block-interet">
-                        <div style="position: relative;">
+                        <div style="position: relative;" id="interet-culture" onclick="etape2_choose('interet-culture')">
                             <img src="https://img.icons8.com/color/100/000000/museum.png">
-                            <img class="icon-validate" src="https://img.icons8.com/cotton/64/000000/cancel.png">
+                            <img id="interet-culture-x" class="icon-validate" src="https://img.icons8.com/cotton/64/000000/cancel.png">
+                            <img id="interet-culture-v" class="icon-validate desactive" src="https://img.icons8.com/cotton/64/000000/checkmark.png">
                         </div>
                         <br>
                         <span>Culture</span>
                     </div>
                     <div class="etape2-block-interet">
-                        <div style="position: relative;">
+                        <div style="position: relative;" id="interet-sport" onclick="etape2_choose('interet-sport')">
                             <img src="https://img.icons8.com/color/100/000000/football2.png">
-                            <img class="icon-validate" src="https://img.icons8.com/cotton/64/000000/cancel.png">
+                            <img id="interet-sport-x" class="icon-validate" src="https://img.icons8.com/cotton/64/000000/cancel.png">
+                            <img id="interet-sport-v" class="icon-validate desactive" src="https://img.icons8.com/cotton/64/000000/checkmark.png">
                         </div>
                         <br>
                         <span>Sport</span>
@@ -85,6 +89,7 @@
 <script type="text/javascript">
 var isSolo = false;
 var isGroup = false;
+var tabInteret = [false, false, false, false];
     function etape1_choose(id)
     {
         if(id == 'etape1-choix-group')
@@ -100,6 +105,61 @@ var isGroup = false;
             isSolo = true;
             $("#etape1-choix-group").attr("class", "etape1-choix");
             $("#etape1-choix-solo").attr("class", "etape1-choix-clicked");
+        }
+    }
+
+    function etape2_choose(id) {
+        switch (id) {
+            case 'interet-food':
+                if (tabInteret[0] == false) {
+                    tabInteret[0] = true;
+                    $("#interet-food-x").attr("class", "icon-validate desactive");
+                    $("#interet-food-v").attr("class", "icon-validate active");
+                } else {
+                    tabInteret[0] = false;
+                    $("#interet-food-x").attr("class", "icon-validate active");
+                    $("#interet-food-v").attr("class", "icon-validate desactive");
+                }
+                break;
+
+            case 'interet-landscape':
+                if (tabInteret[1] == false) {
+                    tabInteret[1] = true;
+                    $("#interet-landscape-x").attr("class", "icon-validate desactive");
+                    $("#interet-landscape-v").attr("class", "icon-validate active");
+                } else {
+                    tabInteret[1] = false;
+                    $("#interet-landscape-x").attr("class", "icon-validate active");
+                    $("#interet-landscape-v").attr("class", "icon-validate desactive");
+                }
+                break;
+
+            case 'interet-culture':
+                if (tabInteret[2] == false) {
+                    tabInteret[2] = true;
+                    $("#interet-culture-x").attr("class", "icon-validate desactive");
+                    $("#interet-culture-v").attr("class", "icon-validate active");
+                } else {
+                    tabInteret[2] = false;
+                    $("#interet-culture-x").attr("class", "icon-validate active");
+                    $("#interet-culture-v").attr("class", "icon-validate desactive");
+                }
+                break;
+
+            case 'interet-sport':
+                if (tabInteret[3] == false) {
+                    tabInteret[3] = true;
+                    $("#interet-sport-x").attr("class", "icon-validate desactive");
+                    $("#interet-sport-v").attr("class", "icon-validate active");
+                } else {
+                    tabInteret[3] = false;
+                    $("#interet-sport-x").attr("class", "icon-validate active");
+                    $("#interet-sport-v").attr("class", "icon-validate desactive");
+                }
+                break;
+        
+            default:
+                break;
         }
     }
 </script>
