1884312d5651dc10b9d5fde5d3d4e6eccc8586c4
BenjaminBoutrois <benjamin.boutrois28@gmail.com>
2019-05-31 11:16:23 +0200

    Ajout étape 1

--- modules/resa.php
+++ modules/resa.php
@@ -14,7 +14,16 @@
         <section id="etape1">
             <h1 class="resa-titre">Etape 1</h1>
             <div class="resa-etape-contenu">
-                <span>Etape 1</span>
+                <div id="etape1-choix-group" class="etape1-choix" onclick="etape1_choose('etape1-choix-group')">
+                    <img src="../img/en_groupe-100.png"/>
+                    <br>
+                    <span class="etape1-label">En groupe</span>
+                </div>
+                <div id="etape1-choix-solo" class="etape1-choix" onclick="etape1_choose('etape1-choix-solo')">
+                    <img src="../img/seul-100.png"/>
+                    <br>
+                    <span class="etape1-label">Seul</span>
+                </div>
             </div>
         </section>
         <section id="etape2">
@@ -37,4 +46,25 @@
         </section>
     </div>
 </body>
+<script type="text/javascript">
+var isSolo = false;
+var isGroup = false;
+    function etape1_choose(id)
+    {
+        if(id == 'etape1-choix-group')
+        {
+            isGroup = true;
+            isSolo = false;
+            $("#etape1-choix-group").attr("class", "etape1-choix-clicked");
+            $("#etape1-choix-solo").attr("class", "etape1-choix");
+        }
+        else if(id == 'etape1-choix-solo')
+        {
+            isGroup = false;
+            isSolo = true;
+            $("#etape1-choix-group").attr("class", "etape1-choix");
+            $("#etape1-choix-solo").attr("class", "etape1-choix-clicked");
+        }
+    }
+</script>
 </html>
